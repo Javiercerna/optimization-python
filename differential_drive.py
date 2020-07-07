a61679dc019bb9d414519a7e552d5eacc27f7cfa
Javiercerna <javier.cerna@pucp.pe>
2020-07-07 16:41:49 +0200

    Refactor differential_drive example w/ NonLinearMPC class

--- differential_drive.py
+++ differential_drive.py
@@ -8,100 +8,143 @@ import numpy as np
 
 import math
 
-N = 80  # prediction horizon
-dt = 0.2  # sampling time
 
-speed_limits = [-0.6, 0.6]
-angular_speed_limits = [-math.pi / 4, math.pi / 4]
-position_limits = [-10, 10]  # Assume symmetric for x and y
+class NonLinearMPC(object):
+    def __init__(self):
+        ## Prediction horizon (in steps)
+        self.N = 80
+        ## Sampling time (in seconds)
+        self.dt = 0.2
+        ## Weights on the state (x, y, angle) for all horizon steps
+        self.Q = casadi.SX([[1, 0, 0],
+                            [0, 1, 0],
+                            [0, 0, 1]])
+        ## Weights on the inputs (speed, angular_speed) for all horizon steps
+        self.R = casadi.SX([[1, 0],
+                            [0, 1]])
+        ## Number of states (x, y angle)
+        self.nx = 3
+        ## Number of inputs (speed, angular_speed)
+        self.nu = 2
+        ## Constraints in the state
+        self.position_limits = [-10, 10]
+        ## Constraints in the inputs
+        self.speed_limits = [-0.6, 0.6]
+        self.angular_speed_limits = [-math.pi / 4, math.pi / 4]
+        ## Casadi solver
+        self.solver = None
+        ## Casadi lower bounds on the state (inputs "u")
+        self.lower_bound_x = [self.speed_limits[0], self.angular_speed_limits[0]] * self.N  # noqa
+        ## Casadi upper bounds on the state (inputs "u")
+        self.upper_bound_x = [self.speed_limits[1], self.angular_speed_limits[1]] * self.N  # noqa
+        ## Casadi function to reconstruct the trajectory from the inputs
+        self.reconstruct_trajectory = None
+
+    def setup_optimization_problem(self):
+        x = casadi.SX.sym('x')
+        y = casadi.SX.sym('y')
+        angle = casadi.SX.sym('angle')
+        speed = casadi.SX.sym('speed')
+        angular_speed = casadi.SX.sym('angular_speed')
+
+        states = [x, y, angle]
+        controls = [speed, angular_speed]
+
+        control_action = [
+            self.dt * speed * casadi.cos(angle),
+            self.dt * speed * casadi.sin(angle),
+            self.dt * angular_speed
+        ]
+
+        f = casadi.Function(
+            'f',
+            states + controls, control_action,
+            ['x', 'y', 'angle', 'speed', 'angular_speed'],
+            ['d_x', 'd_y', 'd_angle']
+        )
+
+        U = casadi.SX.sym('U', self.nu, self.N)
+        P = casadi.SX.sym('P', 2 * self.nx)  # Initial state + reference state
+
+        X = casadi.SX.sym('X', self.nx, self.N + 1)
+
+        X[:, 0] = P[0:self.nx]
+
+        for k in range(self.N):
+            f_values = f(
+                x=X[:, k][0], y=X[:, k][1], angle=X[:, k][2],
+                speed=U[:, k][0], angular_speed=U[:, k][1]
+            )
+            X[:, k + 1] = X[:, k] + casadi.vertcat(*f_values.values())
+
+        J = self._make_cost_function(X, U, P)
+        constraints = self._make_constraints(X)
+
+        nlp = {
+            'x': casadi.reshape(U, 2 * self.N, 1),
+            'f': J,
+            'g': casadi.vertcat(*constraints),
+            'p': P
+        }
+
+        self.solver = casadi.nlpsol('S', 'ipopt', nlp)
+        self.reconstruct_trajectory = self._make_reconstruct_trajectory(
+            X, U, P
+        )
+
+    def compute_optimal_trajectory(self, initial_state, goal_state):
+        initial_inputs = np.zeros((self.N, 2))
+
+        result = self.solver(
+            x0=casadi.reshape(initial_inputs.T, 2 * self.N, 1),
+            lbg=self.position_limits[0],
+            ubg=self.position_limits[1],
+            lbx=self.lower_bound_x,
+            ubx=self.upper_bound_x,
+            p=initial_state + goal_state
+        )
+
+        trajectory = self.reconstruct_trajectory(
+            casadi.vertcat(result['x'], initial_state, goal_state)
+        )
+
+        return trajectory
+
+    def _make_cost_function(self, X, U, P):
+        J = 0
+
+        for k in range(self.N):
+            state_objective = X[:, k] - P[self.nx:]
+            state_cost = state_objective.T @ self.Q @ state_objective
+            input_cost = U[:, k].T @ self.R @ U[:, k]
+            J += state_cost + input_cost
+
+        return J
+
+    def _make_constraints(self, X):
+        constraints = []
+
+        for k in range(self.N + 1):
+            constraints += [X[0, k], X[1, k]]
+
+        return constraints
+
+    def _make_reconstruct_trajectory(self, X, U, P):
+        return casadi.Function(
+            'ff',
+            [casadi.vertcat(casadi.reshape(U, 2 * self.N, 1), P)],
+            [X]
+        )
 
 
-x = casadi.SX.sym('x')
-y = casadi.SX.sym('y')
-angle = casadi.SX.sym('angle')
-speed = casadi.SX.sym('speed')
-angular_speed = casadi.SX.sym('angular_speed')
+if __name__ == '__main__':
+    non_linear_mpc = NonLinearMPC()
 
-states = [x, y, angle]
-controls = [speed, angular_speed]
+    non_linear_mpc.setup_optimization_problem()
 
-nx = len(states)
-nu = len(controls)
+    initial_state = [0, 0, 0]
+    goal_state = [5, 5, 0]
 
-control_action = [
-    dt * speed * casadi.cos(angle),
-    dt * speed * casadi.sin(angle),
-    dt * angular_speed
-]
-
-f = casadi.Function(
-    'f', states + controls, control_action,
-    ['x', 'y', 'angle', 'speed', 'angular_speed'], ['d_x', 'd_y', 'd_angle'])
-
-U = casadi.SX.sym('U', nu, N)
-P = casadi.SX.sym('P', 2 * nx)  # Initial state + reference state
-
-X = casadi.SX.sym('X', nx, N + 1)
-
-X[:, 0] = P[0:nx]
-
-for k in range(N):
-    f_values = f(
-        x=X[:, k][0], y=X[:, k][1], angle=X[:, k][2],
-        speed=U[:, k][0], angular_speed=U[:, k][1]
+    trajectory = non_linear_mpc.compute_optimal_trajectory(
+        initial_state, goal_state
     )
-    X[:, k + 1] = X[:, k] + casadi.vertcat(*f_values.values())
-
-ff = casadi.Function(
-    'ff',
-    [casadi.vertcat(casadi.reshape(U, 2 * N, 1), P)],
-    [X]
-)
-
-J = 0
-constraints = []
-
-Q = casadi.SX([[1, 0, 0],
-               [0, 1, 0],
-               [0, 0, 1]])
-
-R = casadi.SX([[1, 0],
-               [0, 1]])
-
-for k in range(N):
-    state_cost = (X[:, k] - P[nx:]).T @ Q @ (X[:, k] - P[nx:])
-    input_cost = U[:, k].T @ R @ U[:, k]
-    J += state_cost + input_cost
-
-for k in range(N + 1):
-    constraints += [X[0, k], X[1, k]]
-
-
-nlp = {
-    'x': casadi.reshape(U, 2 * N, 1),
-    'f': J,
-    'g': casadi.vertcat(*constraints),
-    'p': P
-}
-
-S = casadi.nlpsol('S', 'ipopt', nlp)
-
-lower_bound_x = [speed_limits[0], angular_speed_limits[0]] * N
-upper_bound_x = [speed_limits[1], angular_speed_limits[1]] * N
-
-initial_state = [0, 0, 0]
-goal_state = [5, 5, 0]
-initial_inputs = np.zeros((N, 2))
-
-result = S(
-    x0=casadi.reshape(initial_inputs.T, 2 * N, 1),
-    lbg=position_limits[0],
-    ubg=position_limits[1],
-    lbx=lower_bound_x,
-    ubx=upper_bound_x,
-    p=initial_state + goal_state
-)
-
-trajectory = ff(casadi.vertcat(result['x'], initial_state, goal_state))
-
-print(trajectory)
